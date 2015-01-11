d569321acf7244534f110876696b1632b7d1e567
sugamasao <sugamasao@gmail.com>
2015-01-11 16:14:59 +0900

    tapを考慮に入れてなかった

--- app_setup/recipe.rb
+++ app_setup/recipe.rb
@@ -1,5 +1,20 @@
 # brew install
-%w(openssl git tig imagemagick mysql ricty zsh tmux mecab mecab-ipadic tree peco).each do |pkg|
+%w(openssl git tig imagemagick mysql zsh tmux mecab mecab-ipadic tree).each do |pkg|
+  package pkg do
+    action :install
+    only_if "brew info #{ pkg } | grep -q 'Not Installed'"
+  end
+end
+
+## using tap repository
+%w(sanemat/font peco/peco).each do |tap_name|
+  execute "using tap #{ tap_name }" do
+    command "brew tap #{ tap_name }"
+    not_if "brew tap | grep -q '#{ tap_name }'"
+  end
+end
+
+%w(ricty peco).each do |pkg|
   package pkg do
     action :install
     only_if "brew info #{ pkg } | grep -q 'Not Installed'"
