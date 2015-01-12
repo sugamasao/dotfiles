951cd0aa9f6895e676da227409bec9a0771f28f8
sugamasao <sugamasao@gmail.com>
2015-01-12 16:42:26 +0900

    line break

--- app_setup/recipe.rb
+++ app_setup/recipe.rb
@@ -1,5 +1,18 @@
 # brew install
-%w(openssl git tig hub imagemagick mysql zsh tmux mecab mecab-ipadic tree poppler).each do |pkg|
+%w(
+  openssl
+  git
+  tig
+  hub
+  imagemagick
+  mysql
+  zsh
+  tmux
+  mecab
+  mecab-ipadic
+  tree
+  poppler
+).each do |pkg|
   package pkg do
     action :install
     only_if "brew info #{ pkg } | grep -q 'Not Installed'"
