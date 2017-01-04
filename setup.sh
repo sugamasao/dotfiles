eec0518ad9c59d90a4beae98f0a037add711c8e4
sugamasao <sugamasao@gmail.com>
2017-01-04 17:10:58 +0900

    add git subcommands

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-SETUP_LIST=(.peco .tmux.conf .vimrc .zsh .zshrc)
-CURRENT_DIR=$(cd $(dirname $0) && pwd)
-
-for file in ${SETUP_LIST[@]};do
-  base=${CURRENT_DIR}/${file}
-  target=${HOME}/${file}
+function symlik_file() {
+  base=${1}
+  target=${2}
 
   if [ -L ${target} ]; then
     echo "${file} is already symlink. (not create symlink)"
@@ -15,5 +12,27 @@ for file in ${SETUP_LIST[@]};do
     ln -s ${base} ${target}
     echo "create symlink 'ln -s ${base} ${target}'"
   fi
+}
+
+###############
+## ~/
+###############
+SETUP_LIST=(.peco .tmux.conf .vimrc .zsh .zshrc)
+CURRENT_DIR=$(cd $(dirname $0) && pwd)
+
+for file in ${SETUP_LIST[@]};do
+  base=${CURRENT_DIR}/${file}
+  target=${HOME}/${file}
+  symlik_file ${base} ${target}
 done
 
+###############
+## ~/bin
+###############
+mkdir -p ~/bin
+for subcommand in `ls ${CURRENT_DIR}/git-subcommands/*`;do
+  file=$(basename ${subcommand})
+  base=${subcommand}
+  target=${HOME}/bin/${file}
+  symlik_file ${base} ${target}
+done
